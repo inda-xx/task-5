785556159dd00a71a4361b9092a05dc3d1c76cf8
Avid <107996421+HotFazz@users.noreply.github.com>
2024-09-27 03:18:55 +0200

    Update Player.java

--- gen_src/Player.java
+++ gen_src/Player.java
@@ -6,15 +6,33 @@ public class Player {
     public static final int[] DIRECTION = {0, 1, 2, 3}; // UP, DOWN, LEFT, RIGHT
     
     public Player() {
-        // TODO: Initialize the player at the starting position (0,0)
+        // Initialize the player at the starting position (0,0)
+        position = new ArrayList<>();
+        position.add(0); // x-coordinate
+        position.add(0); // y-coordinate
     }
 
     public void move(int direction) {
-        // TODO: Move the player based on the direction
+        // Move the player based on the direction
+        switch (direction) {
+            case 0: // UP
+                position.set(1, position.get(1) + 1);
+                break;
+            case 1: // DOWN
+                position.set(1, position.get(1) - 1);
+                break;
+            case 2: // LEFT
+                position.set(0, position.get(0) - 1);
+                break;
+            case 3: // RIGHT
+                position.set(0, position.get(0) + 1);
+                break;
+            default:
+                System.out.println("Invalid Direction");
+        }
     }
     
     public ArrayList<Integer> getPosition() {
-        // TODO: Return the player's position
-        return null;
+        return position;
     }
 }
